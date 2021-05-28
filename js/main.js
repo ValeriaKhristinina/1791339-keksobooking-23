05ff3aab47ae2f258da54ee82355765de82e629d
Valeria Khristinina <valeria.khristinina@gmail.com>
2021-05-28 23:00:38 +0200

    fix quote type in file

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 // https://www.w3schools.com/js/js_random.asp
 const findRandomNumber = (from, to) => {
-  const result = (from >= to || from < 0) ?  'Please, write correct arrange' :  Math.floor(Math.random() * to) + from;
+  const result =
+    from >= to || from < 0
+      ? 'Please, write correct arrange'
+      : Math.floor(Math.random() * to) + from;
   return result;
 };
 
