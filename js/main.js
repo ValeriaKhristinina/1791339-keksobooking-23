b668fbbeb243f07ce2b138febb736786d936ad0d
Valeria Khristinina <valeria.khristinina@gmail.com>
2021-05-28 22:36:45 +0200

    Create function for finding random number

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// https://www.w3schools.com/js/js_random.asp
+const findRandomNumber = (from, to) => {
+  const result = (from >= to || from < 0) ?  'Please, write correct arrange' :  Math.floor(Math.random() * to) + from;
+  return result;
+};
+
+console.log(findRandomNumber(0, 500));
