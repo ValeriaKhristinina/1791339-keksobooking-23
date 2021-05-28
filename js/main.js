db5b11c33aeb53feca413b91a278b608528d00b1
Valeria Khristinina <valeria.khristinina@gmail.com>
2021-05-28 23:58:56 +0200

    Created function for finding random float number

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-console */
+
 // https://www.w3schools.com/js/js_random.asp
 const findRandomNumber = (from, to) => {
   const result =
@@ -8,3 +10,13 @@ const findRandomNumber = (from, to) => {
 };
 
 console.log(findRandomNumber(0, 500));
+
+const findRandomFloatNumber = (from, to, round) => {
+  const result =
+    from >= to || from < 0 || round <= 0
+      ? 'Please, write correct arrange'
+      : (Math.random() * to + from).toFixed(round);
+  return result;
+};
+
+console.log(findRandomFloatNumber(1, 10, 3));
