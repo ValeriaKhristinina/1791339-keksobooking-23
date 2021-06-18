854b4c9fcf5580ab9279808da62820b0864206b6
Valeria Khristinina <valeria.khristinina@gmail.com>
2021-06-18 20:50:03 +0200

    created functions for page states

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
 import './modules/popup.js';
+import {disablePageState, activePageState} from './modules/page-state.js';
+
+
+disablePageState();
+
+window.addEventListener('load', () => {
+  activePageState();
+});
--- js/modules/page-state.js
+++ js/modules/page-state.js
@@ -1,3 +1,23 @@
 /* eslint-disable no-unused-vars */
-const activePageState = () => {};
-const disablePageState = () => {};
+const adForm = document.querySelector('.ad-form');
+const fielsets = adForm.querySelectorAll('fieldset');
+const mapFilter = document.querySelector('.map__filters');
+
+
+const disablePageState = () => {
+  adForm.classList.add('ad-form--disabled');
+  mapFilter.classList.add('map__filters--disabled');
+  fielsets.forEach((element) => {
+    element.setAttribute('disabled', true);
+  });
+};
+
+const activePageState = () => {
+  adForm.classList.remove('ad-form--disabled');
+  mapFilter.classList.remove('map__filters--disabled');
+  fielsets.forEach((element) => {
+    element.setAttribute('disabled', false);
+  });
+};
+
+export { disablePageState, activePageState};
