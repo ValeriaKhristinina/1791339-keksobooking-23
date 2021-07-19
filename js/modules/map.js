e0c19a1bdfa981cebccb754e1c340027091d28ca
Valeria Khristinina <valeria.khristinina@gmail.com>
2021-07-19 21:07:14 +0200

    fixed svg icons

--- js/modules/map.js
+++ js/modules/map.js
@@ -42,7 +42,7 @@ L.tileLayer(
 ).addTo(map);
 
 const mainPinIcon = L.icon({
-  iconUrl: '../../img/main-pin.svg',
+  iconUrl: 'img/main-pin.svg',
   iconSize: [52, 52],
   iconAnchor: [26, 52],
 });
@@ -59,7 +59,7 @@ const mainPinMarker = L.marker(
   },
 );
 const icon = L.icon({
-  iconUrl: '../../img/pin.svg',
+  iconUrl: 'img/pin.svg',
   iconSize: [40, 40],
   iconAnchor: [20, 40],
 });
